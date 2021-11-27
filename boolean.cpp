dd8c818c48ecbce37937fa9870a5d992454e13ea
Odhiambo Fredrick Ochieng <dadidaochieng@gmail.com>
2021-11-27 14:25:48 +0300

    Boolesn to strings

---
+++ boolean.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age1 = 32;
+    int age2 = 20;
+
+    std::cout << std::boolalpha << (age1 > age2) << std::endl;
+    
+    return 0;
+}
